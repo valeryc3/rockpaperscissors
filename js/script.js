1fb32884096e29663f2609a745f145e3df9369c9
valery <valerycorral@ccpaedu.com>
2018-11-08 01:50:42 +0000

    added if statements

--- js/script.js
+++ js/script.js
@@ -14,29 +14,90 @@ var randomNumber = Math.random();
 
 
 // DOCUMENT READY FUNCTION BELOW
-$("button").click(function() {
-  $("#userChoice").text($("#input").val());
-});
 
+console.log($("#userChoice").val())
 if (randomNumber < .33)
 {
       computerChoice= "rock"; 
-      $(computerChoice).css("color", "orange");
 }
 else if (randomNumber > .66)
 {
       computerChoice= "paper"; 
-       $(computerChoice).css("color", "green");
+     
 }
 else
 {
     computerChoice= "scissors";
-     $(computerChoice).css("color", "red");
+  
 }
-  $("#computerChoice").text(computerChoice)   
+$("#shoot").click(function() {
+  $("#userChoice").text($("#input").val());
+  userChoice = $("#input").val();
+  console.log('input', userChoice)
+   $("#computerChoice").text(computerChoice); 
+       if (userChoice == "rock" && computerChoice =="scissors") 
+    {
+        
+            
+             $("#result").text("User Wins");
+             
+       }
+     if (userChoice =="rock" && computerChoice =="paper")
+         {
+          
+             $("#result").text("Computer Wins");    
+         }
+         if (userChoice =="rock" && computerChoice=="rock")
+         {
+          
+             $("#result").text("Tie");    
+         }
+  if(userChoice == "paper" && computerChoice=="rock") 
+    {
+        
+              $("#result").text("User Wins");
+       }
+         else if (userChoice =="paper" && computerChoice=="scissors")
+         {
+          
+           $("#result").text("Computer Wins");   
+         }
+         if (userChoice =="paper" && computerChoice=="paper")
+         {
+          
+             $("#result").text("Tie");    
+         }
+      if(userChoice == "scissors" && computerChoice=="paper") 
+       {
+        
+           
+             $("#result").text("User Wins");
+       }
+         else if (userChoice =="scissors" && computerChoice=="rock")
+         {
+         
+             $("#result").text("Computer Wins"); 
+         }
+         if (userChoice =="scissors" && computerChoice=="scissors")
+         {
+          
+             $("#result").text("Tie");    
+         }
+        userChoice.clear;
+        
+
+});
+
+
+ 
+
+   
 
 
 $("button").hover(function(){
   $("button").css("background-color", "blue") 
    $("button").css("color", "white") 
 })
+
+    console.log('computerChoice', computerChoice)
+    console.log('userChoice', userChoice)
